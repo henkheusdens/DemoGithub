ee1c8608bd6ff5bff7ae87494f7bf6319baac6ac
Henk Heusdens <henk.heusdens@additude.eu>
2020-04-28 12:03:29 +0200

    Add method doStuff()

--- src/TestGithub.java
+++ src/TestGithub.java
@@ -1,5 +1,10 @@
 public class TestGithub {
+
     public static void main(String[] args) {
         System.out.println("Test met Github");
     }
+    public static void doStuff(){
+        System.out.println("Do methode doStuff. ");
+    }
+
 }
